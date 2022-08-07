a76f6979b2e3315dda2ae4eb3c1f896d849dfe25
emekeo <gistwithcollins@gmail.com>
2022-08-07 10:59:28 -0700

    printf update

--- printf.c
+++ printf.c
@@ -42,3 +42,35 @@ int print_op(const char *format, fmt_t *print_arr, va_list list)
 	}
 	return (count);
 }
+
+/**
+ * _printf - prints output according to format
+ * @format: string being passed
+ * Return: char to be printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list list;
+	int a = 0;
+
+	fmt_t ops[] = {
+		{"c", ch},
+		{"s", str},
+		{"d", _int},
+		{"b", _bin},
+		{"i", _int},
+		{"u", _ui},
+		{"o", _oct},
+		{"x", _hex_l},
+		{"X", _hex_u},
+		{"R", _rot13},
+		{NULL, NULL}
+	};
+
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	a = print_op(format, ops, list);
+	va_end(list);
+	return (a);
+}
