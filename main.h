a18334bb60f2c48aa5f486af01da02cefede853d
emekeo <gistwithcollins@gmail.com>
2022-08-08 00:13:55 -0700

    update main file

--- main.h
+++ main.h
@@ -1,32 +1,26 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
-/**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
- */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
+#include <stdlib.h>
 
+/**
+ * struct printer - structure for printing to stdout, given a specifier
+ * @specifier: the specifier recognized by this printer (without the %)
+ * @run: the function to run when this printer is invoked
+ */
+typedef struct printer
+{
+	char *specifier;
+	int (*run)(va_list);
+} printer;
+
+printer _get_printer(const char *specifier);
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int _print_char(va_list);
+int _print_str(va_list);
+int _print_decimal(va_list);
+int _print_int(va_list);
 
-#endif  /* _MAIN_H */
+#endif /* MAIN_H */
