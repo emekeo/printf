d7f26364dacc0f2ff1f389519953d8a8baed0312
emekeo <gistwithcollins@gmail.com>
2022-08-08 02:14:30 -0700

    update main.h

--- main.h
+++ main.h
@@ -1,26 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdarg.h>
-#include <stdlib.h>
-
 /**
- * struct printer - structure for printing to stdout, given a specifier
- * @specifier: the specifier recognized by this printer (without the %)
- * @run: the function to run when this printer is invoked
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-typedef struct printer
+typedef struct print
 {
-	char *specifier;
-	int (*run)(va_list);
-} printer;
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-printer _get_printer(const char *specifier);
 int _putchar(char c);
 int _printf(const char *format, ...);
-int _print_char(va_list);
-int _print_str(va_list);
-int _print_decimal(va_list);
-int _print_int(va_list);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 
-#endif /* MAIN_H */
+#endif  /* _MAIN_H */
